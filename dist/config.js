8d61150c22360cacaa8319ac64f6057ed388c290
holmirzayevboburjon <holmirzayevboburjon2002@gmail.com>
2026-09-09 12:37:58 +0500

    Connect thank-you subscription button to Telegram invite

--- dist/config.js
+++ dist/config.js
@@ -1,2 +1,2 @@
 // Telegram kanal havolasini shu yerga yozing, masalan: https://t.me/kanal_nomi
-window.SITE_CONFIG = Object.freeze({ telegramUrl: "", registrationEndpoint: "/api/register" });
+window.SITE_CONFIG = Object.freeze({ telegramUrl: "https://t.me/+pI8qgctAozFjZTIy", registrationEndpoint: "/api/register" });
